9dd5335e228916d610f6bfd6c7d952ae51798aa6
Piotr Zielinski <piotr.zielinski@inwedo.com>
2025-04-25 23:21:13 +0200

    Init migration

---
+++ Paragony/Migrations/20250425211744_Init.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Paragony.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Receipts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    StoreName = table.Column<string>(type: "text", nullable: false),
+                    PurchaseDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "numeric", nullable: false),
+                    ReceiptNumber = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Receipts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReceiptItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Quantity = table.Column<decimal>(type: "numeric", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    ReceiptId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReceiptItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReceiptItems_Receipts_ReceiptId",
+                        column: x => x.ReceiptId,
+                        principalTable: "Receipts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("27653b89-3b92-4ace-87c3-f025dcab07b3"), "Alkohol", "Alkohol" },
+                    { new Guid("27dfb8ce-f64a-442d-b3a8-9a767dd83a0e"), "Artykuły domowe", "Dom" },
+                    { new Guid("4df2457f-af33-4c31-8b70-86db6bdc35aa"), "Artykuły spożywcze", "Jedzenie" },
+                    { new Guid("7223583b-ac12-4db7-8043-b9d81427ff9e"), "Inne", "Inne" },
+                    { new Guid("882e9888-f64b-41e9-99bc-2c9f52bc79c3"), "Środki czystości i higieny", "Chemia" },
+                    { new Guid("ab5bdd86-8bf1-4ed4-8ee1-9a2b9e6995a9"), "Odzież i obuwie", "Ubrania" },
+                    { new Guid("d3225f27-b658-4696-b2b8-68c7670e2cd7"), "Urządzenia elektroniczne", "Elektronika" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceiptItems_ReceiptId",
+                table: "ReceiptItems",
+                column: "ReceiptId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "ReceiptItems");
+
+            migrationBuilder.DropTable(
+                name: "Receipts");
+        }
+    }
+}
