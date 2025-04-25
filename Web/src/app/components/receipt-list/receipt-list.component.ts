30d2188b5176cb57cd8d77c2908c19cef1ad2cc4
bartek.gorski <bartek.gorski@inwedo.com>
2025-04-26 01:54:24 +0200

    open chat on click

--- Web/src/app/components/receipt-list/receipt-list.component.ts
+++ Web/src/app/components/receipt-list/receipt-list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ChatBoxComponent } from '../chat-box/chat-box.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 interface Receipt {
   date: string;
@@ -10,12 +13,27 @@ interface Receipt {
 @Component({
   selector: 'app-receipt-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDialogModule],
   templateUrl: './receipt-list.component.html',
   styleUrls: ['./receipt-list.component.scss']
 })
 export class ReceiptListComponent {
   dateRange: string = '01.04-01.05.2025';
+
+  constructor(private dialog: MatDialog) {}
+
+  openVoiceChat() {
+    const dialogRef = this.dialog.open(ChatBoxComponent, {
+      width: '90%',
+      height: '80%',
+      panelClass: 'voice-chat-dialog',
+      data: { startRecording: true }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Chat dialog closed');
+    });
+  }
 
   receipts: Receipt[] = [
     { date: '23-04-2025', store: 'Lidl', amount: 123.46 },
